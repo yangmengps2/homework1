be7ecba28554c3a8a6229a02a2ca7ed150c9cf95
Ian <ian.chen@snowyhydro.com.au>
2025-12-29 13:16:47 +1100

    add s3 and dynamodb

--- infra-tf/backend.tf
+++ infra-tf/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "ianc-terraform-state  "
+    bucket         = "ianc-terraform-state"
     key            = "homework1/terraform.tfstate"
     region         = "ap-southeast-2"
-    dynamodb_table = "REPLACE_ME_LOCK_TABLE"
+    dynamodb_table = "ianc-terraform-lock"
     encrypt        = true
   }
 }
