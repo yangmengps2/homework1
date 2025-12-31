c98b43713df954904885ccb40983bc85e173b769
Ian <ian.chen@snowyhydro.com.au>
2025-12-31 23:55:36 +1100

    assign cluster admin to my users and roles

--- infra-tf/modules/eks/cluster.tf
+++ infra-tf/modules/eks/cluster.tf
@@ -14,6 +14,10 @@ resource "aws_eks_cluster" "this" {
     endpoint_private_access = false
   }
 
+  access_config {
+    authentication_mode = "API_AND_CONFIG_MAP"
+  }
+
   enabled_cluster_log_types = ["api", "audit", "authenticator"]
 
   depends_on = [
