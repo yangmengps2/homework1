9b3fc22c6529af58c5bc7d61b26bd685ac6d86c7
Ian <ian.chen@snowyhydro.com.au>
2026-01-01 01:44:51 +1100

    output ca

--- infra-tf/versions.tf
+++ infra-tf/versions.tf
@@ -2,6 +2,14 @@ terraform {
   required_version = ">= 1.6.0"
 
   required_providers {
+    helm = {
+      source  = "hashicorp/helm"
+      version = "~> 2.13"  # 任选一个你想固定的 2.x
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 2.29"
+    }
     aws = {
       source  = "hashicorp/aws"
       version = ">= 5.0"
