1166862d72b08a2ccf5e9af0fb12ba3f219c4eac
Ian <ian.chen@snowyhydro.com.au>
2026-01-01 20:02:10 +1100

    add irsa-oidc.tf

---
+++ infra-tf/irsa-oidc.tf
@@ -0,0 +1,13 @@
+data "tls_certificate" "eks_oidc" {
+  url = data.aws_eks_cluster.this.identity[0].oidc[0].issuer
+}
+
+resource "aws_iam_openid_connect_provider" "eks" {
+  url = data.aws_eks_cluster.this.identity[0].oidc[0].issuer
+
+  client_id_list = ["sts.amazonaws.com"]
+
+  thumbprint_list = [
+    data.tls_certificate.eks_oidc.certificates[0].sha1_fingerprint
+  ]
+}
