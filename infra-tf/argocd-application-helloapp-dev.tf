d1c51cd20ca799ba0337e22da30ffae7dca7275b
Ian <ian.chen@snowyhydro.com.au>
2026-01-02 23:07:02 +1100

    update image updater annotations

--- infra-tf/argocd-application-helloapp-dev.tf
+++ infra-tf/argocd-application-helloapp-dev.tf
@@ -8,12 +8,19 @@ resource "kubernetes_manifest" "argocd_app_helloapp_dev" {
 
       # 这些注解先放着：等我们装 image-updater 后就会生效
       annotations = {
-        "argocd-image-updater.argoproj.io/image-list"                 = "helloapp=939503809934.dkr.ecr.ap-southeast-2.amazonaws.com/helloapp"
-        "argocd-image-updater.argoproj.io/helloapp.update-strategy"   = "latest"
-        "argocd-image-updater.argoproj.io/helloapp.allow-tags"        = "regexp:^sha-[0-9a-f]{7}$"
-        "argocd-image-updater.argoproj.io/write-back-method"          = "git"
-        "argocd-image-updater.argoproj.io/write-back-target"          = "kustomization"
-        "argocd-image-updater.argoproj.io/git-branch"                 = "main"
+        "argocd-image-updater.argoproj.io/image-list"               = "helloapp=939503809934.dkr.ecr.ap-southeast-2.amazonaws.com/helloapp"
+        "argocd-image-updater.argoproj.io/helloapp.update-strategy" = "latest"
+
+        # 放宽到 7~40 位，避免以后 tag 变长就匹配不到
+        "argocd-image-updater.argoproj.io/helloapp.allow-tags"      = "regexp:^sha-[0-9a-f]{7,40}$"
+
+        # 关键：让 updater 基于 main 写到 dev 分支
+        "argocd-image-updater.argoproj.io/git-branch"               = "main:dev"
+
+        # 关键：用 secret 的 git 写权限（你后面会创建这个 secret）
+        "argocd-image-updater.argoproj.io/write-back-method"        = "git:secret:argocd/helloapp-gitops-write-creds"
+
+        "argocd-image-updater.argoproj.io/write-back-target"        = "kustomization"
       }
     }
 
