a44b9809a06b4a15e0e2f2eb6295eb74e1ab90e9
Ian <ian.chen@snowyhydro.com.au>
2026-01-03 20:40:33 +1100

    add karpenter

--- infra-tf/helm-karpenter.tf
+++ infra-tf/helm-karpenter.tf
@@ -7,6 +7,43 @@ resource "helm_release" "karpenter_crd" {
   repository = "oci://public.ecr.aws/karpenter"
   chart      = "karpenter-crd"
 
+
   # 版本你先不要写死（让它用最新），跑通后我们再 pin 版本
   # version  = "x.y.z"
+}
+
+resource "helm_release" "karpenter" {
+  name      = "karpenter"
+  namespace = "karpenter"
+
+  repository = "oci://public.ecr.aws/karpenter"
+  chart      = "karpenter"
+
+  # 我们用了独立的 CRD chart
+  skip_crds = true
+
+  # 确保 CRD 先装好
+  depends_on = [helm_release.karpenter_crd]
+
+  set {
+    name  = "serviceAccount.annotations.eks\\.amazonaws\\.com/role-arn"
+    value = "arn:aws:iam::939503809934:role/helloapp-dev-karpenter-controller-role"
+  }
+
+  set {
+    name  = "settings.clusterName"
+    value = module.eks.cluster_name
+  }
+
+  # queue name（不是 ARN）
+  set {
+    name  = "settings.interruptionQueue"
+    value = "helloapp-dev-karpenter-interruption"
+  }
+
+  # 你刚创建的 instance profile name（不是 ARN）
+  set {
+    name  = "settings.aws.defaultInstanceProfile"
+    value = "helloapp-dev-karpenter-node-instance-profile"
+  }
 }
